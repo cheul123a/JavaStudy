64b528cfde6cad06e3faef70ebaff47ddd4f22b9
Kim MinKyu <cheul123a@naver.com>
2018-09-04 12:28:37 +0900

    변수 선언 및 사용

---
+++ study/src/chapter01/VariableExample.java
@@ -0,0 +1,11 @@
+package chapter01;
+
+public class VariableExample {
+	public static void main(String[] args) {
+		int value = 10;					//변수 value에 10을 저장
+		int result = value + 10;		//변수 result에 value값에 10을 더한 값을 저장
+		
+		System.out.println(result);
+	}
+
+}
