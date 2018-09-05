5af475f380e6b70f488963e69e2da346c5695521
Kim MinKyu <cheul123a@naver.com>
2018-09-05 12:14:14 +0900

    사다리꼴 넓이 구하기

---
+++ study/src/chapter02/Exercise03.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int height = 7;
+		double area = (double)((lengthTop + lengthBottom) * height) / 2;
+		System.out.println(area);
+	}
+
+}
