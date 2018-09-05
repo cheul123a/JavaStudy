b25c7e4d33aca5c0c7b0cd453831c3290708ce12
Kim MinKyu <cheul123a@naver.com>
2018-09-05 12:00:23 +0900

    연필 개수 구하기

---
+++ study/src/chapter02/Exercise01.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int pencils = 534;
+		int students = 30;
+		
+		int pencilsPerStudent = pencils / students;
+		System.out.println(pencilsPerStudent);
+		
+		int LeftPencils = pencils % students;
+		System.out.println(LeftPencils);
+
+	}
+
+}
