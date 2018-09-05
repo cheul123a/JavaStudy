153b13d2b05cc6abc3f95b7c5411e3f8a3a83bbf
Kim MinKyu <cheul123a@naver.com>
2018-09-05 12:10:10 +0900

    문제 2

---
+++ study/src/chapter02/Exercise02.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+public class Exercise02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value =356;
+		System.out.println(value -= value % 100);
+	}
+
+}
