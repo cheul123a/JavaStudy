1a30c0dfb26f7306b12ed7791b1f328e38ffcc94
Kim MinKyu <cheul123a@naver.com>
2018-09-05 17:00:31 +0900

    키보드로 while문 제어

---
+++ study/src/chapter03/WhileKeyControlExample.java
@@ -0,0 +1,37 @@
+package chapter03;
+
+public class WhileKeyControlExample {
+
+	public static void main (String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		boolean run = true;
+		int speed = 0;
+		int keyCode=  0;
+		
+		while(run) {
+			if(keyCode!=13 && keyCode!=10) {
+			System.out.println("---------------");
+			System.out.println("1.증속 | 2.감속 | 3.중지");
+			System.out.println("---------------");
+			System.out.println("선택 : ");
+			}
+			keyCode = System.in.read();
+			
+			switch(keyCode) {
+			case 49:
+				speed++;
+				System.out.println("현재속도 : " +speed);
+				break;
+			case 50:
+				speed--;
+				System.out.println("현재속도 : " +speed);
+				break;
+			case 51:
+				run = false;
+				break;
+			}
+		}
+		System.out.println("프로그램 종료");
+	}
+
+}
