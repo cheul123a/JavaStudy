199d1c75b1c0692449a786803515c42cb6c17313
Kim MinKyu <cheul123a@naver.com>
2018-09-07 14:31:52 +0900

    향상된 for문

---
+++ study/src/chapter04/AdvancedForExample.java
@@ -0,0 +1,20 @@
+package chapter04;
+
+public class AdvancedForExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] scores = {95, 71, 84, 93, 87};
+		
+		int sum = 0;
+		for (int score : scores) {
+			sum += score;
+		}
+		System.out.println("Á¡¼ö ÃÑÇÕ  : " + sum);
+		
+		double avg = (double)sum / scores.length;
+		
+		System.out.println("Æò±Õ : " +avg);
+	}
+
+}
