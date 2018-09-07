7ded1050117a2f2b76a6788bb466b5dd1d1e0120
Kim MinKyu <cheul123a@naver.com>
2018-09-07 11:37:25 +0900

    배열복사

---
+++ study/src/chapter04/ArrayCopyByForSystemArrayExample.java
@@ -0,0 +1,32 @@
+package chapter04;
+
+public class ArrayCopyByForSystemArrayExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] oldIntArray = {1, 2, 3};
+		int[] newIntArray = new int[5];
+		
+		for(int i = 0; i<oldIntArray.length; i++) {
+			newIntArray[i] = oldIntArray[i];
+		}
+		
+		for(int i = 0; i<newIntArray.length; i++) {
+			System.out.print(newIntArray[i]+", ");
+		}
+		
+		System.out.println();
+		
+		String[] oldStrArray = {"java", "array", "copy"};
+		String[] newStrArray = new String [5];
+		
+		System.arraycopy(oldStrArray, 0, newStrArray, 0, oldStrArray.length);
+		
+		for( int i= 0 ; i<newStrArray.length; i++) {
+			System.out.print(newStrArray[i]+", ");
+		}
+		
+		
+	}
+
+}
