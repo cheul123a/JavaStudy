8a499f7b79d330e89db3c34652b0f219f623288e
Kim MinKyu <cheul123a@naver.com>
2018-09-10 16:36:10 +0900

    클래스 내부에서 메소드 호출

---
+++ study/src/chapter05/ClassMethodExample.java
@@ -0,0 +1,11 @@
+package chapter05;
+
+public class ClassMethodExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassMethod Calc = new ClassMethod();
+		Calc.execute();
+	}
+
+}
