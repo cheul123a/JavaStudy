86a5419ca2c15f6e0c4b1bb17cdb3b3a68c7fa76
Kim MinKyu <cheul123a@naver.com>
2018-09-10 11:14:52 +0900

    필드 자동 초기화

---
+++ study/src/chapter05/FieldInitValue.java
@@ -0,0 +1,22 @@
+package chapter05;
+
+public class FieldInitValue {
+
+	byte byteField;
+	short shortField;
+	int intField;
+	long longField;
+	
+	boolean booleanField;
+	char charField;
+	
+	float floatField;
+	double doubleField;
+	
+	int[] arrField;
+	String referenceField;
+	
+	
+	
+	
+}
