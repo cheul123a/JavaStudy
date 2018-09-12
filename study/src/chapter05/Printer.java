8af53948130a43869c3744b06bc43b4cd5348ecc
Kim MinKyu <cheul123a@naver.com>
2018-09-12 14:49:40 +0900

    확인문제 16, 17

---
+++ study/src/chapter05/Printer.java
@@ -0,0 +1,20 @@
+package chapter05;
+
+public class Printer {
+
+	public static void println(int thing) {
+		System.out.println(thing);
+	}
+
+	public static void println(double thing) {
+		System.out.println(thing);
+	}
+
+	public static void println(boolean thing) {
+		System.out.println(thing);
+	}
+
+	public static void println(String thing) {
+		System.out.println(thing);
+	}
+}
---
+++ study/src/chapter05/PrinterExample.java
@@ -0,0 +1,15 @@
+package chapter05;
+
+public class PrinterExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Printer.println(20);
+		Printer.println(true);
+		Printer.println(5.87);
+		Printer.println("ȫ�浿");
+	}
+
+}
