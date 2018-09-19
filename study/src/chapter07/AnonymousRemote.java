7e89f81b7c31230334235832c608c7337c2993a4
Kim MinKyu <cheul123a@naver.com>
2018-09-19 12:50:03 +0900

    익명구현객체

---
+++ study/src/chapter07/AnonymousRemote.java
@@ -0,0 +1,21 @@
+package chapter07;
+
+public class AnonymousRemote {
+
+	public static void main(String[] args) {
+		RemoteControl rc = new RemoteControl() {
+			
+			@Override
+			public void turnOn() {
+			}
+			
+			@Override
+			public void turnOff() {
+			}
+			
+			@Override
+			public void setVolume(int volume) {
+			}
+		};
+	}
+}
