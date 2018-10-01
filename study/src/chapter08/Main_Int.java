7b604475c8c608cec2f349409bfea1ce7de4575f
Kim MinKyu <cheul123a@naver.com>
2018-10-01 17:10:38 +0900

    익명 구현 객체

---
+++ study/src/chapter08/Main_Int.java
@@ -0,0 +1,12 @@
+package chapter08;
+
+public class Main_Int {
+
+	public static void main(String[] args) {
+
+		Window w = new Window();
+		w.button1.touch();
+		w.button2.touch();
+	}
+
+}
---
+++ study/src/chapter08/Person.java
@@ -0,0 +1,8 @@
+package chapter08;
+
+public class Person {
+
+	void wake() {
+		System.out.println("7시에 일어납니다.");
+	}
+}
