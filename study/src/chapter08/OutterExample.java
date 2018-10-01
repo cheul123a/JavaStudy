5632a95e67f7d4151e52b8a1d36a292e6d8463e0
Kim MinKyu <cheul123a@naver.com>
2018-10-01 12:00:39 +0900

    중첩 클래스에서 바깥 클래스 참조 얻기

---
+++ study/src/chapter08/OutterExample.java
@@ -0,0 +1,11 @@
+package chapter08;
+
+public class OutterExample {
+
+	public static void main(String[] args) {
+		Outter outter = new Outter();
+		Outter.Nested nested = outter.new Nested();
+		nested.Print();
+	}
+
+}
