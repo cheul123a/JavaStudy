56787970cc8eedcf4ccabd9e312231779a612d35
Kim MinKyu <cheul123a@naver.com>
2018-10-12 16:38:27 +0900

    객체의 문자 정보(toString() 메소드)

---
+++ study/src/chapter10/ToStringExample.java
@@ -0,0 +1,16 @@
+package chapter10;
+
+import java.util.Date;
+
+public class ToStringExample {
+
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Date ojb2 = new Date();
+		
+		System.out.println(obj1.toString());
+		System.out.println(ojb2.toString());
+		
+	}
+
+}
