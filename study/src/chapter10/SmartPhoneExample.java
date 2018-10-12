5022575cfa6f7f567fc42b7aa36daf7386d0e3e6
Kim MinKyu <cheul123a@naver.com>
2018-10-12 17:32:29 +0900

    객체의 문자 정보(toString() 메소드)

---
+++ study/src/chapter10/SmartPhoneExample.java
@@ -0,0 +1,20 @@
+package chapter10;
+
+public class SmartPhoneExample {
+
+	public static void main(String[] args) {
+
+		SmartPhone myPhone = new SmartPhone("Samsung", "GalaxyNote 5");
+		
+		String strObj = myPhone.toString();
+		
+		System.out.println(strObj);
+		
+		System.out.println(myPhone);
+		
+		
+		
+		
+	}
+
+}
